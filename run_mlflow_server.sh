a5a2e85d6ed5e1ddd100b349177f62d6453b5832
Sergey Anufriev <sergii.anufriiev@gmail.com>
2024-07-21 11:00:57 +0000

    mlflow_tutoriual_servers

---
+++ run_mlflow_server.sh
@@ -0,0 +1,15 @@
+
+export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
+export AWS_ACCESS_KEY_ID=YCAJE8CKXGh8XKUmcmGHpV2CX
+export AWS_SECRET_ACCESS_KEY=YCNQmCkW71PeIVypo2YsF-4eIHhiRBZZN94pztrj
+export AWS_BUCKET_NAME=s3-student-mle-20240620-7af3548300
+export DB_DESTINATION_USER=mle_20240620_7af3548300
+export DB_DESTINATION_HOST=rc1b-uh7kdmcx67eomesf.mdb.yandexcloud.net
+export DB_DESTINATION_PASSWORD=bac9e597d2fc43bea26785ec829ddd14
+export DB_DESTINATION_PORT=6432
+export DB_DESTINATION_NAME=playground_mle_20240620_7af3548300
+
+mlflow server \
+  --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
+    --default-artifact-root s3://$AWS_BUCKET_NAME \
+    --no-serve-artifacts
